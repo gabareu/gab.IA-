9862724fc0c78bdb5186d3f8142c8708c085d07e
gabareu <dyck.gabriel@escola.pr.gov.br>
2025-09-25 20:55:24 -0300

    ok

--- script.js
+++ script.js
@@ -136,11 +136,31 @@ const perguntas = [
         perguntaAtual=perguntas[atual];
         caixaPerguntas.textContent = perguntaAtual.enunciado;
         caixaAlternativas.textContent="";
-        mostraAlternativa();
+        mostraAlternativas();
     }
 
     function mostraAlternativas(){
-        for(const alternativa of perguntaAtual.alternativas)
+    for(const alternativa of perguntaAtual.alternativas) {
+        const botaoAlternativas = document.createElement("button");
+        botaoAlternativas.textContent = alternativa.texto;
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild(botaoAlternativas);
+}
+
+function respostaSelecionada(opcaoSelecionada) {
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
+
+function mostraResultado() {
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
+
+mostraPergunta();
     }
 
 
